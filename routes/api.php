f1eab93f97fca78e89756a732057c80369c57ef9
LeoPavlovski <lp29379@seeu.edu.mk>
2023-06-24 16:00:27 +0200

    feat:[Create Users, Drivers , Trips Routes]

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\DriverController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\TripController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('auth:sanctum')->group(function(){
+   //User
+    Route::get('/getUsers', [UserController::class, 'index']);
+    Route::get('/getUser/{user}',[UserController::class , 'show']);
+    Route::post('/createUser',[UserController::class, 'store']);
+    Route::put('/updateUser/{user}',[UserController::class, 'update']);
+    Route::delete('/deleteUser/{user}',[UserController::class, 'destroy']);
+    //Drivers
+    Route::get('/getDrivers', [DriverController::class, 'index']);
+    Route::get('/getDriver/{driver}',[DriverController::class , 'show']);
+    Route::post('/createDriver',[DriverController::class, 'store']);
+    Route::put('/updateDriver/{driver}',[DriverController::class, 'update']);
+    Route::delete('/deleteDriver/{driver}',[DriverController::class, 'destroy']);
+    //Trips
+    Route::get('/getTrips', [TripController::class, 'index']);
+    Route::get('/getTrip/{trip}',[TripController::class , 'show']);
+    Route::post('/createTrip',[TripController::class, 'store']);
+    Route::put('/updateTrip/{trip}',[TripController::class, 'update']);
+    Route::delete('/deleteTrip/{trip}',[TripController::class, 'destroy']);
+});
+
+
+Route::post('/login',[LoginController::class , 'submit']);
+Route::post('/login/verify',[LoginController::class, 'verify']);
