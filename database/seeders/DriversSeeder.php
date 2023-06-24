6ed417bb486b5f199aa745879fac4f2d2766ae0c
LeoPavlovski <lp29379@seeu.edu.mk>
2023-06-24 16:01:28 +0200

    feat:[Removed the user Id, from the drivers table]

--- database/seeders/DriversSeeder.php
+++ database/seeders/DriversSeeder.php
@@ -11,37 +11,39 @@ class DriversSeeder extends Seeder
     /**
      * Run the database seeds.
      *       'user_id',
+    'user_id',
     'year',
     'model',
     'license_plate',
-    'color'
+    'color',
+
+    'trip_id'
      */
     public function run(): void
     {
         DB::table('drivers')->insert([
             [
-              'user_id'=>1,
               'year'=>2013,
               'model'=>'Aston Martin',
               'license_plate'=>"1234MKD",
               'color'=>'White'
             ],
             [
-                'user_id'=>2,
+
                 'year'=>2004,
                 'model'=>'Opel',
                 'license_plate'=>"1234MKD",
                 'color'=>'White'
             ],
             [
-                'user_id'=>3,
+
                 'year'=>2011,
                 'model'=>'Mitsubishi',
                 'license_plate'=>"1234MKD",
                 'color'=>'White'
             ],
             [
-                'user_id'=>4,
+
                 'year'=>2007,
                 'model'=>'Ford',
                 'license_plate'=>"1234MKD",
