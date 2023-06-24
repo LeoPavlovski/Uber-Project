5f056059ef10ad0cce8980aaf1412bb262d50a18
LeoPavlovski <lp29379@seeu.edu.mk>
2023-06-24 16:05:56 +0200

    feat:[Added TripResource.php]

---
+++ app/Http/Resources/TripResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TripResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *   $table->unsignedBigInteger('user_id');
+    $table->foreign('user_id')->references('id')->on('users');
+    $table->unsignedBigInteger('driver_id');
+    $table->foreign('driver_id')->references('id')->on('drivers');
+    $table->boolean('is_started')->default(false);
+    $table->boolean('is_completed')->default(false);
+    $table->json('origin')->nullable();
+    $table->json('destination')->nullable();
+    $table->string('destination_name')->nullable();
+    $table->json('driver_location')->nullable();
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'user_id' => $this->user_id,
+            'driver_id' => $this->driver_id,
+            'is_started' => $this->is_started,
+            'is_completed' => $this->is_completed,
+             'origin'=>$this->origin,
+            'destination' => $this->destination,
+            'destination_name' => $this->destination_name,
+            'driver_location' => $this->driver_location
+        ];
+    }
+}
