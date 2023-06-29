c057de85a3551206fe0e5ba3ba9bc82c47662417
LeoPavlovski <lp29379@seeu.edu.mk>
2023-06-29 22:20:38 +0200

    Changes to the Drivers ( added user_id , and associated user to be displayed in the Drivers Resource)

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,18 @@
 <template>
 <div class="pt-16">
   <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
-  <form action="#">
+<!--  function-->
+<!--  Preventing any default manners by the -->
+  <form action="#" @submit.prevent="handleLogin">
     <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
       <div class="bg-white px-4 py-5 sm:p-6">
 <!--Validation here-->
         <div>
-          <input type="text" v-maska data-maska="+###########" name="phone" id="phone" placeholder="1 (234) 567-8910" class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm">
+          <input type="text" v-maska data-maska="+389########" name="phone" v-model="phone" id="phone" placeholder="1 (234) 567-8910" class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm">
         </div>
       </div>
       <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-        <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-black pl-5 pr-5 pt-3 pb-3 text-white" >
+        <button type="submit" @submit.prevent="handleLogin" class="inline-flex justify-center rounded-md border border-transparent bg-black pl-5 pr-5 pt-3 pb-3 text-white" >
           Continue
         </button>
       </div>
@@ -23,6 +25,22 @@
 <script setup>
 //Functionality
 import {vMaska} from 'maska'
+import {reactive, ref} from "vue"
+import axios from 'axios'
+// difference is that we are going to access only the phone
+const phone = ref(null);
+//storing the phone
+//better approach
+const data = reactive({
+  phone:null
+})
+const handleLogin =()=>{
+  //instead of the console.log
+  //do axios.post (route to the api)
+  axios.post('http://localhost/api/login')
+  console.log(data.phone)
+}
+
 </script>
 
 <style>
