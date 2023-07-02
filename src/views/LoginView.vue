b1258447ab0effe0063e43e07e216237c56a750d
LeoPavlovski <lp29379@seeu.edu.mk>
2023-07-03 01:02:31 +0200

    fix (prev. google maps)

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,110 +1,90 @@
 <template>
-<div class="pt-16">
-  <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
-<!--  function-->
-<!--  Preventing any default manners by the -->
-  <form v-if="!waitingOnVerification" action="#" @submit.prevent="handleLogin">
-    <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
-      <div class="bg-white px-4 py-5 sm:p-6">
- <!--Validation here-->
-        <div>
-          <input type="text" v-maska data-maska="+389########" name="phone" v-model="credentials.phone" id="phone" placeholder="1 (234) 567-8910" class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm">
+  <div class="pt-16">
+    <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
+    <form v-if="!waitingOnVerification" action="#" @submit.prevent="handleLogin">
+      <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+        <div class="bg-white px-4 py-5 sm:p-6">
+          <div>
+            <input type="text" v-maska data-maska="+389########" v-model="credentials.phone" name="phone" id="phone" placeholder="+38912332145"
+                   class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
+          </div>
+        </div>
+        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+          <button type="submit" @submit.prevent="handleLogin"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Continue</button>
         </div>
       </div>
-      <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-        <button type="submit" @submit.prevent="handleLogin" class="inline-flex justify-center rounded-md border border-transparent bg-black pl-5 pr-5 pt-3 pb-3 text-white" >
-          Continue
-        </button>
-      </div>
-    </div>
-  </form>
-
-    <form v-else action="#" @submit.prevent="handleVerification" >
-        <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
-            <div class="bg-white px-4 py-5 sm:p-6">
-                <!--Validation here-->
-                <div>
-                    <input type="text" v-maska data-maska="######" name="login_code" v-model="credentials.login_code" id="login_code" placeholder="134512" class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm">
-                </div>
-            </div>
-            <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                <button type="submit" @submit.prevent="handleVerification" class="inline-flex justify-center rounded-md border border-transparent bg-black pl-5 pr-5 pt-3 pb-3 text-white" >
-                   Verify
-                </button>
-            </div>
-        </div>
     </form>
+    <form v-else action="#" @submit.prevent="handleVerification">
+      <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+        <div class="bg-white px-4 py-5 sm:p-6">
+          <div>
+            <input type="text" v-maska data-maska="######" v-model="credentials.login_code" name="login_code" id="login_code" placeholder="123456"
+                   class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
+          </div>
         </div>
-
+        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+          <button type="submit" @submit.prevent="handleVerification"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Verify</button>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
-
 <script setup>
-//Functionality
-import {vMaska} from 'maska'
-import {computed, onMounted, reactive, ref} from "vue"
+import { vMaska } from 'maska'
+import { reactive, ref, onMounted, computed } from 'vue'
 import axios from 'axios'
-//router to navigate the user to various routes
-import router from "@/router";
-// difference is that we are going to access only the phone
-const phone = ref(null);
-const waitingOnVerification = ref(false);
+import { useRouter } from 'vue-router';
 
-// onMounted(()=>{
-//     if (localStorage.getItem('token') && !waitingOnVerification.value) {
-//         router.push({
-//             name: 'LandingView'
-//         });
-//     }
-// });
+const router = useRouter()
 
-//Computed : generate reactive properties based on the values on different values
-
-//storing the phone
-//better approach
 const credentials = reactive({
-  phone:null,
-  login_code:null
+  phone: null,
+  login_code: null
 })
-const getFormatedCredentials = ()=>{
-    return {
-        phone:credentials.phone.replaceAll(' ', '').replace('(','').replace(')','').replace('-',''),
-        login_code : credentials.login_code
-    }
+
+const waitingOnVerification = ref(false)
+
+onMounted(() => {
+  if (localStorage.getItem('token')) {
+    router.push({
+      name: 'landing'
+    })
+  }
+})
+
+const getFormattedCredentials = () => {
+  return {
+    phone: credentials.phone.replaceAll(' ', '').replace('(', '').replace(')', '').replace('-', ''),
+    login_code: credentials.login_code
+  }
 }
-const handleLogin =()=>{
-  //instead of the console.log
-  //do axios.post (route to the api)
-  axios.post('http://localhost:8000/api/login',
-      getFormatedCredentials()
-)
-      .then((response)=>{
+
+const handleLogin = () => {
+  axios.post('http://localhost:8000/api/login', getFormattedCredentials())
+      .then((response) => {
         console.log(response.data)
-          waitingOnVerification.value=true;
-      }).catch((error)=>{
-        console.log(error)
+        waitingOnVerification.value = true
+      })
+      .catch((error) => {
+        console.error(error)
         alert(error.response.data.message)
-      });
-  // console.log(data.phone)
+      })
 }
-const handleVerification = ()=>{
-    axios.post('http://localhost:8000/api/login/verify',
-        getFormatedCredentials()
-    )
-        .then((response)=>{
-            console.log(response.data); //auth token
-            localStorage.setItem('token',response.data) // Token from the console.
-            //Determine if they want to be a driver or a passenger
-            // TODO determine if the user has been already been logged in to the page
-            router.push({
-                name:'index'
-            })
+
+const handleVerification = () => {
+  axios.post('http://localhost:8000/api/login/verify', getFormattedCredentials())
+      .then((response) => {
+        console.log(response.data) // auth token
+        localStorage.setItem('token', response.data)
+        router.push({
+          name: 'location'
         })
-        .catch((error)=>{
-            alert(error)
-        });
+      })
+      .catch((error) => {
+        console.error(error)
+        alert(error.message)
+      })
 }
 </script>
-
-<style>
-
-</style>
